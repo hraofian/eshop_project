ca0e6136667000336a1d34da6bd7837efbe08642
hadi <hadi@hadi.com>
2024-05-12 08:34:46 +0330

    4th commit

--- product_module/models.py
+++ product_module/models.py
@@ -1,6 +1,6 @@
-from django.db import models
+# from django.db import models
 
 
-class Product(models.Model)
-    title = models.CharField(max_length=300)
-    price = models.IntegerField()
+# class Product(models.Model)
+#     title = models.CharField(max_length=300)
+#     price = models.IntegerField()
