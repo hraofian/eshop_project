0480bae206b5979e8f567bca313b12d2a7fe4e65
hadi <hadi@hadi.com>
2024-05-13 14:29:22 +0330

    9th commit- video 46

--- product_module/models.py
+++ product_module/models.py
@@ -1,6 +1,8 @@
+from typing import Iterable
 from django.db import models
 from django.core.validators import MinValueValidator , MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Product(models.Model):
@@ -9,14 +11,14 @@ class Product(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)] , default=0)
     short_description = models.CharField(max_length=360 , null=True)
     is_active = models.BooleanField(default=False)
+    slug = models.SlugField(default="" , null=False , db_index=True)
 
     def get_absolute_url(self):
-        return reverse('product-detail' , args=[self.id])
+        return reverse('product-detail' , args=[self.slug])
     
-    # def get_absolute_url(self):
-    #     return reverse("product_detail", kwargs={"pk": self.pk})
-    
-    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} -  {self.price}"
--- product_module/views.py
+++ product_module/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render , get_object_or_404
 from .models import Product
 from django.http import Http404
+from django.db.models import Avg , Min , Max
 
 def product_list(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by("title")
+    number_of_product = products.count()
+    avg_rating = products.aggregate(Avg("rating") , Min("price"), Max("id"))
     return render(request , 'product_module/product_list.html', {
-        'products': products
+        'products': products,
+        'total_number_of_product':number_of_product,
+        'avaeage_rating':avg_rating,
     })
 
-# def product_detail(request , product_id):
-#     product = Product.objects.get(id=product_id)
-#     return render(request, 'product_module/product_detail.html', {
-#         'product': product
-#     })
 
-def product_detail(request , product_id):
-    product = get_object_or_404(Product , pk=product_id)
+
+def product_detail(request , slug):
+    product = get_object_or_404(Product , slug=slug)
     return render(request, 'product_module/product_detail.html', {
-        'product': product
+        'product': product,
+
     })
