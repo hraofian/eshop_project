d5b51d40c1919fa10af62ee450d856b0876a0900
hadi <hadi@hadi.com>
2024-05-13 12:03:40 +0330

    8th commit -video 42

--- product_module/views.py
+++ product_module/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from .models import Product
+from django.http import Http404
 
 def product_list(request):
     products = Product.objects.all()
@@ -7,9 +8,14 @@ def product_list(request):
         'products': products
     })
 
+# def product_detail(request , product_id):
+#     product = Product.objects.get(id=product_id)
+#     return render(request, 'product_module/product_detail.html', {
+#         'product': product
+#     })
+
 def product_detail(request , product_id):
-    product = Product.objects.get(id=product_id)
+    product = get_object_or_404(Product , pk=product_id)
     return render(request, 'product_module/product_detail.html', {
         'product': product
     })
-
