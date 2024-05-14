a88aa60cfeac792e6a36ffbb91f2621531e80de4
hadi <hadi@hadi.com>
2024-05-14 14:38:05 +0330

    12th commit - video 58

--- product_module/admin.py
+++ product_module/admin.py
@@ -8,8 +8,15 @@ class ProductAdmin(admin.ModelAdmin):
         'slug':['title']
     }
 
-    list_display = ['__str__' , 'title' , 'price' , 'is_active']
+    list_display = ['__str__' , 'title' , 'price' , 'is_active', 'category']
     list_filter = ['price' , 'is_active']
     list_editable = ['title' , 'is_active' , 'price']
 
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display=['__str__' ,'title' , 'url_title']
+
 admin.site.register(models.Product , ProductAdmin)
+
+admin.site.register(models.ProductCategory , ProductCategoryAdmin)
+admin.site.register(models.ProductInformation)
+admin.site.register(models.ProductTag)
--- product_module/models.py
+++ product_module/models.py
@@ -4,17 +4,57 @@ from django.core.validators import MinValueValidator , MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
 
+
+class ProductTag(models.Model):
+    tag = models.CharField(max_length=300 , verbose_name='تگ محصول')
+
+    class Meta:
+        verbose_name = 'تگ محصول'
+        verbose_name_plural= 'تگ های محصولات'
+
+    def __str__(self):
+        return self.tag
+
+
+
+
 class ProductCategory(models.Model):
     title = models.CharField(max_length=300 , verbose_name='عنوان')
     url_title = models.CharField(max_length=300 , verbose_name='عنوان در url')
 
     def __str__(self):
         return self.title
+    
+    class Meta:
+        verbose_name ='دسته بندی'
+        verbose_name_plural = 'دسته بندی ها'
+
+class ProductInformation(models.Model):
+    color = models.CharField(max_length=200 , verbose_name='رنگ')
+    size = models.CharField(max_length=200 , verbose_name='اندازه')
+
+    def __str__(self):
+        return f'({self.color} - {self.size} )'
+    
+    class Meta:
+        verbose_name ='اطلاعات تکمیلی محصول '
+        verbose_name_plural = 'اطلاعات تکمیلی محصولات'
 
 
 class Product(models.Model):
     title = models.CharField(max_length=300)
-    category = models.ForeignKey(ProductCategory , on_delete=models.CASCADE , null=True)
+    product_information = models.OneToOneField(
+    ProductInformation, 
+    on_delete=models.CASCADE, 
+    related_name='product_information',
+    null=True,
+    blank=True)
+    category = models.ForeignKey(ProductCategory 
+    , on_delete=models.CASCADE , 
+    null=True , 
+    related_name='products',
+    verbose_name='دسته بندی')
+    product_tags = models.ManyToManyField(ProductTag)
     price = models.IntegerField()
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)] , default=0)
     short_description = models.CharField(max_length=360 , null=True)
@@ -30,6 +70,10 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.title} -  {self.price}"
+    
+    class Meta:
+        verbose_name ='محصول '
+        verbose_name_plural = 'محصولات  '
 
 
     
