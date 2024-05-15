1c620d2c40b764b062e30a99cb64836d77ae22d2
hadi <hadi@hadi.com>
2024-05-15 14:33:37 +0330

    15th commit - use render partial - video 62

--- home_module/urls.py
+++ home_module/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('' , views.index_pade, name='product-list'),
+    path('contact-us' , views.contact_page, name='contact-page'),
+
 ]
--- home_module/views.py
+++ home_module/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def index_pade(request):
     return render(request , 'home_module/index_page.html')
+
+
+def contact_page(request):
+    return render(request , 'home_module/contact_page.html')
+
+
+
+def site_header_component(request):
+    context = {
+        'menue_link': 'آموزش جنگو'
+    }
+    return render(request , 'shared/site_header_component.html', context)
+
+
+
+def site_footer_component(request):
+    return render(request , 'shared/site_footer_component.html')
+
