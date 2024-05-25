dc365a259804a467c37912b7ae766a84ae759020
hadi <hadi@hadi.com>
2024-05-25 21:36:35 +0330

    32th commit video 72

--- contact_module/views.py
+++ contact_module/views.py
@@ -17,14 +17,16 @@ def contact_us_page(request):
         contact_form = ContactUsModelForm(request.POST)
 
         if contact_form.is_valid():
-            print(contact_form.cleaned_data)
-            contact = ContactUs(
-                title=contact_form.cleaned_data.get('title'),
-                full_name=contact_form.cleaned_data.get('full_name'),
-                email=contact_form.cleaned_data.get('email'),
-                message=contact_form.cleaned_data.get('message'),
-            )
-            contact.save()
+            # print(contact_form.cleaned_data)
+            # contact = ContactUs(
+            #     title=contact_form.cleaned_data.get('title'),
+            #     full_name=contact_form.cleaned_data.get('full_name'),
+            #     email=contact_form.cleaned_data.get('email'),
+            #     message=contact_form.cleaned_data.get('message'),
+            # )
+            # contact.save()
+            contact_form.save()
+
             return redirect('home_page')
     # contact_form = ContactUsForm()   
     contact_form = ContactUsModelForm()   
--- eshop_project/urls.py
+++ eshop_project/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('' , include('home_module.urls')),
     path('contact-us/', include('contact_module.urls')),
     path('products/' , include('product_module.urls')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls , name='myadmin'),
 
 
 ]
